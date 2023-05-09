50be0cd0e91e5f78cff36e7788f2037379ad1630
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-05-09 23:35:41 +0200

    feat: add backgroud-image, create and display DishForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import DishForm from './components/dishForm/dishForm'
 
 const App = () => {
 	return (
-		<div className="App">
-		</div>
+		<DishForm />
 	)
 }
 
---
+++ src/components/dishForm/dishForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './dishForm.module.scss'
+
+
+const DishForm = () => {
+
+	return (
+		<div className={styles.layout}>
+			<div className={styles.dishForm}>
+
+			</div>
+		</div>
+	)
+}
+export default DishForm
