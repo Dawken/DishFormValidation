51eddef3c40b3a860dfc2eede8c469b27fad42d9
Dawid Domiński <dawid.dominski1125@gmail.com>
2023-05-11 01:44:13 +0200

    feat: add enum to type dish and regex to preparation time in dishSchema

--- src/components/dishForm/useDishForm.ts
+++ src/components/dishForm/useDishForm.ts
@@ -1,15 +1,18 @@
-import { object, string, TypeOf } from 'zod'
+import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {useForm} from 'react-hook-form'
+import {TypeOf} from 'zod'
 
 
-const dishSchema = object({
-	name: string()
+const dishSchema = z.object({
+	name: z.string()
 		.nonempty('Dish name is required'),
-	preparation_time: string()
-		.nonempty('Preparation time is required'),
-	type: string()
-		.nonempty('Dish type is required')
+	preparation_time: z.string()
+		.nonempty('Preparation time is required')
+		.refine((value) => /^\d{2}:\d{2}:\d{2}$/.test(value), {
+			message: 'Preparation time must be in the format HH:MM:SS',
+		}),
+	type: z.enum(['Pizza', 'Soup', 'Sandwich'])
 })
 
 type DishInput = TypeOf<typeof dishSchema>
